01913e91a4c80a7d7a44717072fd6f05c8c28058
Sudhansh Yelishetty <44008102+ysudhansh@users.noreply.github.com>
2020-09-10 22:40:16 +0530

    Add files via upload

---
+++ trajectories.m
@@ -0,0 +1,35 @@
+function trajectories()
+
+ego_gt = importdata("4.txt");
+vehicle_poses = importdata("MobiliTraj_4_2_17-43.txt");
+base_ego = ego_gt(18,:);
+r = base_ego(1:9);
+R_ego = reshape(r, [3 3]);
+t_ego = base_ego(10:12)';
+T_base = [R_ego, t_ego; zeros(1,3) 1];
+% poses = [reshape(R', [1 9], t'];
+ego_motion = [];
+vehicle_motion = [];
+for i=18:44
+    current_ego = ego_gt(i,:);
+    r_ego = current_ego(1:9);
+    R_ego = reshape(r_ego, [3 3]);
+    t_ego = current_ego(10:12)';
+    T_curr = [R_ego t_ego; zeros(1,3) 1];
+    T_ego = inv(T_base) * T_curr;
+    ego_motion = [ego_motion; reshape(T_ego(1:3,1:3)', [1 9]), T_ego(1:3,4)'];
+    current_vehicle = vehicle_poses(i-17,:);
+    r_vehicle = current_vehicle(1:9);
+    R_vehicle = reshape(r_vehicle, [3 3])';
+    t_vehicle = current_vehicle(10:12)';
+    T_vehicle = [R_vehicle t_vehicle; zeros(1,3) 1];
+    T_final = T_ego * T_vehicle;
+    vehicle_motion = [vehicle_motion; reshape(T_final(1:3,1:3)', [1 9]), T_final(1:3,4)'];
+end
+system("rm 4_ego_17-43.txt 4_2_17-43.txt");
+writematrix(ego_motion, "4_ego_17-43.txt", "Delimiter", "space");
+writematrix(vehicle_motion, "4_2_17-43.txt", "Delimiter", "space");
+plot(ego_motion(:,10), ego_motion(:,12), "g", vehicle_motion(:,10), vehicle_motion(:,12), "r");
+
+end
+
